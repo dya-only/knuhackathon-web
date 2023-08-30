4ffa14a0c6ab14ce731421114e53a501fc6ac250
손보석 <dya_only@sonboseog-ui-MacBookPro.local>
2023-08-30 09:27:49 +0900

    FEAT: Click marker

--- src/components/KakaoMap/component.tsx
+++ src/components/KakaoMap/component.tsx
@@ -8,18 +8,34 @@ import Marker from '../../assets/imgs/marker.png'
 export default function KakaoMap (props: any) {
   const [map, setMap] = useState(null as any)
   const [selected, setSelected] = useState(false)
+  const [markerInfo, setMarkerInfo] = useState('')
 
   const getCafeData = async () => {
     const container = document.getElementById('map') as HTMLElement
     const options = { center: new kakao.maps.LatLng(props.coords.latitude, props.coords.longitude), level: 4 }
     const kakaoMap = new kakao.maps.Map(container, options)
-    setMap(kakaoMap)    
+    setMap(kakaoMap)
+    console.log(map)
 
     // Current my location
-    new kakao.maps.Marker({
+    const me = new kakao.maps.Marker({
       map: kakaoMap,
       position: new kakao.maps.LatLng(props.coords.latitude, props.coords.longitude),
       image: new kakao.maps.MarkerImage(Marker, new kakao.maps.Size(30, 40)),
+    })
+
+    kakao.maps.event.addListener(me, 'click', () => {
+      setSelected(true)
+      setMarkerInfo('Me!')
+    })
+
+    kakao.maps.event.addListener(kakaoMap, 'click', () => {
+      setSelected(false)
+    })
+
+    kakao.maps.event.addListener(kakaoMap, 'dblclick', function(event: { latLng: { latitude: number, longitude: number } }) {
+      const latlng = event.latLng
+      console.log(latlng)
     })
   }
 
@@ -30,7 +46,12 @@ export default function KakaoMap (props: any) {
     return (
       <Fragment>
         { selected ? 
-          <div className='fixed z-30 w-screen h-screen bg-black/25 lg:hidden md:hidden' onClick={() => { setSelected(false) } } />
+          <div className={`${selected ? 'animatedBG' : ''} fixed z-50 w-screen h-screen lg:hidden md:hidden`} onClick={() => { setSelected(false) } } />
+        : null }
+        { selected ? 
+          <div className={`${selected ? 'animated' : 'unanimated'} rounded-t-[20px] overflow-y-auto fixed z-50 flex justify-center items-start shadow-2xl bg-white md:w-[400px] lg:w-[400px] w-screen lg:h-screen md:h-screen h-[50%] mt-[120%] lg:mt-0 md:mt-0`}>
+            <p>{ markerInfo }</p>
+          </div>
         : null }
 
         <Nav />
--- src/components/Nav/component.tsx
+++ src/components/Nav/component.tsx
@@ -1,6 +1,6 @@
 export default function Nav () {
   return (
-    <nav className='fixed flex items-center w-screen h-15 rounded-b-2xl bg-white/70 drop-shadow-xl backdrop-blur-2xl p-4 z-50'>
+    <nav className='fixed flex items-center w-screen h-15 rounded-b-2xl bg-white/70 drop-shadow-xl backdrop-blur-2xl p-4 z-40'>
       <p className='text-lg text-blue-400 font-cafe24'>편할지도📍</p>
     </nav>
   )
